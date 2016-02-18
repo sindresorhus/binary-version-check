2fad6b84bf41639f350af977ad53b89f68c516cf
Sindre Sorhus <sindresorhus@gmail.com>
2016-02-18 18:07:28 +0700

    update for latest AVA

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
 test('error when the range does not satisfy the bin version', async t => {
 	try {
-		await fn('curl', '1.29.0');
+		await m('curl', '1.29.0');
 	} catch (err) {
 		t.is(err.name, 'InvalidBinVersion');
 	}
 });
 
 test('no error when the range satisfies the bin version', async t => {
-	await t.doesNotThrow(fn('curl', '>=1'));
+	await t.notThrows(m('curl', '>=1'));
 });
